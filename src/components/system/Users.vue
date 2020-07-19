193eb36f312e5e6308d9b23a5fa73ea798b8d1fc
Mr.s <1561971578@qq.com>
2020-07-20 00:40:22 +0800

    宋斌前台更新

--- src/components/system/Users.vue
+++ src/components/system/Users.vue
@@ -1,26 +1,26 @@
 <template>
   <div>
     <el-breadcrumb separator="/">
-      <el-breadcrumb-item :to="{ path: '/welcome' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{ path:'/welcome'}">首页</el-breadcrumb-item>
       <el-breadcrumb-item>系统管理</el-breadcrumb-item>
       <el-breadcrumb-item>用户列表</el-breadcrumb-item>
     </el-breadcrumb>
     <el-card class="box-card">
       <el-row :gutter="20">
         <el-col :span="8">
-          <el-input placeholder="请输入用户名" v-model="queryInfo.username">
+          <el-input placeholder="请输入用户名称" v-model="queryInfo.username">
             <el-button slot="append" icon="el-icon-search" @click="getUserList"></el-button>
           </el-input>
         </el-col>
         <el-col :span="4">
-          <el-button type="primary" @click="addDialogVisible=true" v-has-permission="'sys:user:add'">新增</el-button>
+          <el-button type="primary" @click="addDialogVisible=true">新增</el-button>
         </el-col>
       </el-row>
       <el-table stripe :data="userList" style="width: 100%">
-        <el-table-column prop="username" label="姓名"></el-table-column>
-        <el-table-column prop="phone" label="电话"></el-table-column>
-        <el-table-column prop="email" label="邮件"></el-table-column>
-        <el-table-column prop="isvaild" label="是否有效">
+        <el-table-column prop="username" label="姓名" width="180"></el-table-column>
+        <el-table-column prop="email" label="EMail" width="180"></el-table-column>
+        <el-table-column prop="phone" label="电话" width="180"></el-table-column>
+        <el-table-column label="状态">
           <template slot-scope="scope">
             <el-switch v-model="scope.row.isvaild" @change="updateUserState(scope.row)"></el-switch>
           </template>
@@ -28,22 +28,19 @@
         <el-table-column fixed="right" label="操作" width="180">
           <template slot-scope="scope">
             <el-button @click="openEditUser(scope.row)" type="primary" size="small" class="el-icon-edit"></el-button>
-            <el-button type="danger" size="small" class="el-icon-delete" @click="delUserBuId(scope.row.id)"></el-button>
-            <el-button type="warning" size="small" class="el-icon-setting" @click="openAssignRole(scope.row.id)"></el-button>
+            <el-button type="danger" size="small" class="el-icon-delete" @click="delUserById(scope.row.id)"></el-button>
+            <el-button type="warning" size="small" class="el-icon-s-tools" @click="openAssignRole(scope.row.id)"></el-button>
           </template>
         </el-table-column>
       </el-table>
-      <el-pagination
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        :current-page="queryInfo.page"
-        :page-sizes="[5, 10, 15, 30]"
-        :page-size="queryInfo.size"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="total">
+      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
+                     :current-page="queryInfo.page"
+                     :page-sizes="[5, 10, 15, 30]" :page-size="queryInfo.size"
+                     layout="total, sizes, prev, pager, next, jumper"
+                     :total="total">
       </el-pagination>
     </el-card>
-    <el-dialog title="新增用户" :visible.sync="addDialogVisible" width="50%" @close="closeAdd">
+    <el-dialog title="新增用户信息" :visible.sync="addDialogVisible" width="50%" @close="closeAdd">
       <el-form :model="addForm" :rules="rulesAdd" ref="ruleFormAdd" label-width="100px">
         <el-form-item label="姓名" prop="username">
           <el-input v-model="addForm.username"></el-input>
@@ -77,13 +74,9 @@
     <el-button type="primary" @click="editUser">确 定</el-button>
   </span>
     </el-dialog>
-    <el-dialog title="分配角色" :visible.sync="roleDialogVisible" width="55%" center>
-      <el-transfer
-        style="text-align: left; display: inline-block"
-        v-model="roleIds"
-        :titles="['未拥有', '已拥有']"
-        :button-texts="['到左边', '到右边']"
-        :data="roles">
+    <el-dialog title="分配角色" :visible.sync="roleDialogVisible" width="60%" center>
+      <el-transfer style="text-align: left; display: inline-block" v-model="roleIds" :titles="['未拥有', '已拥有']"
+                   :button-texts="['到左边', '到右边']" :data="roles">
       </el-transfer>
       <span slot="footer" class="dialog-footer">
     <el-button @click="roleDialogVisible = false">取 消</el-button>
@@ -92,7 +85,6 @@
     </el-dialog>
   </div>
 </template>
-
 <script>
 export default {
   name: 'Users',
@@ -101,14 +93,9 @@ export default {
   },
   methods: {
     async assignRole () {
-      /*
-      *方式一
-      const { data: res } = await this.$http.put('sys/userRole/' + this.userId, { roleIds: this.roleIds.join(',') })
-      */
-
-      /*
-      *方式二
-      */
+      // 方式一
+      // const { data: res } = await this.$http.put('sys/userRole/' + this.userId + '/' + this.roleIds.join(','))
+      // 方式二
       const { data: res } = await this.$http.put('sys/userRole/' + this.userId + '/' + (this.roleIds.join(',') || 'aa'))
       if (res.code !== 200) {
         return this.$message.error(res.msg)
@@ -164,7 +151,7 @@ export default {
         }
       })
     },
-    delUserBuId (userId) {
+    delUserById (userId) {
       this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
@@ -213,6 +200,7 @@ export default {
   },
   data () {
     // 验证邮箱的规则
+    // eslint-disable-next-line no-unused-vars
     var checkEmail = (rule, value, cb) => {
       // 验证邮箱的正则表达式
       const regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/
@@ -226,6 +214,7 @@ export default {
     }
 
     // 验证手机号的规则
+    // eslint-disable-next-line no-unused-vars
     var checkMobile = (rule, value, cb) => {
       // 验证手机号的正则表达式
       const regMobile = /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/
@@ -241,20 +230,43 @@ export default {
       roles: [],
       roleIds: [],
       roleDialogVisible: false,
-      editDialogVisible: false,
-      editForm: {},
+      editForm: {
+      },
       rulesAdd: {
         username: [
-          { required: true, message: '请输入用户姓名', trigger: 'blur' },
-          { min: 2, max: 10, message: '长度在 2 到 10 个字符', trigger: 'blur' }
+          {
+            required: true,
+            message: '请输入用户姓名',
+            trigger: 'blur'
+          },
+          {
+            min: 2,
+            max: 5,
+            message: '长度在 2 到 10 个字符',
+            trigger: 'blur'
+          }
         ],
         email: [
-          { required: true, message: '请输入邮箱', trigger: 'blur' },
-          { validator: checkEmail, trigger: 'blur' }
+          {
+            required: true,
+            message: '请输入邮箱',
+            trigger: 'blur'
+          },
+          {
+            validator: checkEmail,
+            trigger: 'blur'
+          }
         ],
         phone: [
-          { required: true, message: '请输入电话', trigger: 'blur' },
-          { validator: checkMobile, trigger: 'blur' }
+          {
+            required: true,
+            message: '请输入电话',
+            trigger: 'blur'
+          },
+          {
+            validator: checkMobile,
+            trigger: 'blur'
+          }
         ]
       },
       addForm: {
@@ -263,13 +275,14 @@ export default {
         phone: ''
       },
       addDialogVisible: false,
+      editDialogVisible: false,
       total: 0,
       queryInfo: {
         username: '',
         page: 1,
-        size: 10
+        size: 5
       },
-      userList: []
+      userList: [{}]
     }
   }
 }
